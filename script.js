2f655759f3d7ebaedc3e6bde2d8cc07e93884928
Matheus <math.reis.dev@gmail.com>
2021-04-12 16:26:43 -0300

    JS done

---
+++ script.js
@@ -0,0 +1,20 @@
+const questions = [
+  {
+    question: "What is your name? How old are you? How old are you?",
+    example: "Matheus",
+  },
+  {
+    question: "How old are you?",
+    example: "27",
+  },
+  {
+    question: "Where are you from?",
+    example: "Berlin",
+  },
+];
+
+const randomQuest = () => {
+  let random = questions[Math.floor(Math.random() * questions.length)];
+  document.getElementById("question").innerHTML = random.question;
+  document.getElementById("example").innerHTML = random.example;
+};
