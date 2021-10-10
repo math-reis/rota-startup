44063202dfac4afc9c2d194ca5a5f63685a4b5cb
Matheus Reis <math.reis.dev@gmail.com>
2021-10-10 09:22:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Collapse
+
+const coll = document.getElementsByClassName("collapsible");
+let i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    const content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
